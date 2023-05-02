cad9e5d72cfa9563879cda7be422174d40e4fd40
hforn <132222528+hforn@users.noreply.github.com>
2023-05-01 17:26:28 -0700

    Update corsproxy.php

--- corsproxy.php
+++ corsproxy.php
@@ -1,13 +1,34 @@
 <?php
-// Allow cross-origin requests
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 
-// Check if the request method is POST
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  // Get the token from the request body
-  $token = file_get_contents('php://input');
-
-  // TODO: Validate the token and store it in a session or database
+// Only allow POST requests
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+  header('HTTP/1.1 405 Method Not Allowed');
+  header('Allow: POST');
+  echo 'This endpoint only accepts HTTP POST requests';
+  exit;
 }
+
+// Set CORS headers to allow requests from any origin
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type');
+
+// Get the request data from the client-side
+$request_data = file_get_contents('php://input');
+
+// Send the request to the target URL
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $request_data->url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($request_data->data));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+
+// Send the response back to the client-side
+echo $response;
+
+// Validate and store the One Tap Sign-in token
+$token = file_get_contents('php://input');
+// TODO: Validate the token and store it in a session or database
 ?>
